7dfa25c38eec70b27bfb9887f5fd3d829d0bfccc
saraauc <s.mohamed2209@gmail.com>
2024-06-11 05:42:13 -0400

    This is A class mini-practice

---
+++ class.py
@@ -0,0 +1,16 @@
+class HighScores:
+    def __init__(self,scores):
+        self.scores=scores
+    def latest(self):
+        length_list=len(self.scores)
+        latest_score=self.scores[length_list-1]
+        return latest_score
+    def personal_top_three(self):
+        new_list=sorted(self.scores)
+        return sorted(new_list[-3:],reverse=True)
+    def personal_best(self):
+        max_score=0
+        for score in self.scores:
+            if (score>max_score):
+                max_score=score
+        return max_score
