f13922dd18e61febdd83d344748b3f3e7e2882a4
saraauc <s.mohamed2209@gmail.com>
2024-06-24 04:09:25 -0400

    Properties of numbers

---
+++ prime_factor.py
@@ -0,0 +1,13 @@
+def prime_factor(n):
+    factors = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+        if divisor * divisor > n:
+            if n > 1:
+                factors.append(n)
+                break
+    return factors
